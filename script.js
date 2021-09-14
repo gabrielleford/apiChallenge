6fb0b4d9469844c7b811247593568450e65eb9bc
Gabrielle Ford <gabrielledeva@gmail.com>
2021-09-14 16:23:19 -0400

    Working on only grabbing certain gifs

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
 let getQuote = 'https://type.fit/api/quotes';
+let getGif = 'https://api.giphy.com/v1/gifs/search';
+let key = 'ZBdicDFEt4oO7j5Gbi2EFNWNucggrmpq';
 
 let btn = document.querySelector('button');
 let quote = document.getElementById('quote');
 let author = document.getElementById('author');
+let gifDiv = document.getElementById('js');
 
 btn.addEventListener('click', fetchQuotes);
+btn.addEventListener('click', fetchGif);
 
 let randNum;
+let randReact;
+let randGif;
+let gifUrl;
 
 function fetchQuotes(e) {
+
+
     fetch(getQuote)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(json) {
-        console.log(json);
-        displayQuote(json);
-    })
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(json) {
+            displayQuote(json);
+        })
+}
+
+function fetchGif(e) {
+    randReact = Math.floor(Math.random() * 2 + 1);
+    
+    if (randReact === 1) {
+        gifUrl = `${getGif}?api_key=${key}&q=applause&rating=g`;
+    } else {
+        gifUrl = `${getGif}?api_key=${key}&q=impressed&rating=g`;
+    }
+
+    fetch(gifUrl)
+        .then(function(gifResponse) {
+            return gifResponse.json();
+        })
+        .then(function(gifJson) {
+            console.log(gifJson.data);
+            displayGif(gifJson);
+        })
 }
 
 function displayQuote(json) {
     randNum = Math.floor(Math.random() * json.length);
-    console.log(randNum);
 
     let qText = json[randNum].text;
     let qAuthor = json[randNum].author;
@@ -33,4 +59,27 @@ function displayQuote(json) {
     } else {
         author.innerText =`- ${qAuthor}`;
     }
+}
+
+function displayGif(gifJson) {
+    while (gifDiv.children[2]) {
+        gifDiv.removeChild(gifDiv.children[2]);
+    }
+
+    let gif = document.createElement('img');
+
+    randGif = Math.floor(Math.random() * gifJson.data.length);
+    console.log(randGif);
+    let id = gifJson.data[randGif].id;
+
+    if (id === "mLGnUCverTQ52" || id === "3oz8xJcPzeRutjp2BW" || id === "d2ajvwESx7pTO") {
+        randGif += 1;
+        console.log(`New number: ${randGif}`);
+    }
+    
+
+    console.log(gifJson.data[randGif]);
+    gif.src = gifJson.data[randGif].images.original.url;
+
+    gifDiv.appendChild(gif);
 }
